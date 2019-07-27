e4ff775c4bb7b4a945616d176a5c6f04d848e44e
Bascy <bas@basbex.com>
2019-07-27 17:16:34 +0200

    Added I-Connect sensor implementation

---
+++ INA219ShuntSensor.cpp
@@ -0,0 +1,55 @@
+// 
+// 
+// 
+
+#include "INA219ShuntSensor.h"
+#include "SenseUtils.h"
+
+INA219aShuntSensor::INA219aShuntSensor(uint8_t sensorId) : SenseI2CBusSensor(sensorId) {
+}
+
+INA219aShuntSensor::~INA219aShuntSensor() {
+}
+
+float INA219aShuntSensor::getValueAsFloat() {
+  return lastValues.getCurrent_mA();
+}
+
+char * INA219aShuntSensor::getValueAsChars(char * value) {
+ return lastValues.asChars(value);
+}
+
+void INA219aShuntSensor::readRawValue() {
+#define SAMPLES 10.0
+  float sv = 0;
+  float bv = 0;
+  float c = 0;
+  for (uint8_t i = 0; i < trunc(SAMPLES); i++) {
+    sv =+ ina219.getShuntVoltage_mV();
+    bv += ina219.getBusVoltage_V();
+    c += ina219.getCurrent_mA();
+    delay(200);
+  }
+  lastValues.setShuntVoltage_mV(sv / SAMPLES);
+  lastValues.setBusVoltage_V(bv / SAMPLES);
+  lastValues.setCurrent_mA(c / SAMPLES);
+  csSerialPrint(F("Current: "));
+  csSerialPrint(lastValues.getCurrent_mA());
+  csSerialPrintln(" mA");
+}
+
+
+void INA219aShuntSensor::initialize() {
+  ina219.begin();
+  ina219.setCalibration_Ext_16V_75mV_50A();
+  sensorActive = true;
+}
+
+byte * INA219aShuntSensor::getValueInBuffer(byte * buffer) {
+  memcpy(buffer, &lastValues, sizeof(ShuntValue));
+  return buffer;
+}
+
+byte INA219aShuntSensor::getMinBufferSize() {
+  return sizeof(ShuntValue);
+}
---
+++ INA219ShuntSensor.h
@@ -0,0 +1,35 @@
+// INA219ShuntSensor.h
+#pragma once
+
+#include "arduino.h"
+#include <string>
+#include "SenseI2CBusSensor.h"
+#include "ISenseShuntValued.h"
+#include "Adafruit_INA219.h"
+
+
+/*
+  I2C address: 0x40
+*/
+class INA219aShuntSensor : public SenseI2CBusSensor
+{
+public:
+  INA219aShuntSensor(uint8_t sensorId);
+  ~INA219aShuntSensor();
+
+  void initialize() override;
+  float getValueAsFloat() override;
+  char* getValueAsChars(char* value) override;
+  byte* getValueInBuffer(byte* buffer) override;
+  byte getMinBufferSize() override;
+  ShuntValue* getLastValues() { return &lastValues; };
+  void setValueFromBLE(std::string&) override {};
+
+protected:
+  void readRawValue() override;
+
+private:
+  Adafruit_INA219 ina219;
+  ShuntValue lastValues;
+};
+
